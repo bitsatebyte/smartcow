58c4731d21f1c0d6da17a398a298c8c7a3eab42f
Hari Veturi <hariveturi@gmail.com>
2021-02-23 23:51:44 +0530

    ADD: Boilerplate imports for test files

--- src/components/Box/Box.test.js
+++ src/components/Box/Box.test.js
@@ -1,8 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react';
-import Box from './Box';
 
-test('renders box element correctly', () => {
-  render(<Box />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import Box from './Box';
---
+++ src/components/Home/Home.test.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
