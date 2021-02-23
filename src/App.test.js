957ea1c7df3ea782a8f0561266112253c3b6432c
Hari Veturi <hariveturi@gmail.com>
2021-02-24 02:51:06 +0530

    FIX: Remove navbar test from app test file. It needs to be in home test file

--- src/App.test.js
+++ src/App.test.js
@@ -7,11 +7,3 @@ it("Renders without crashing", () => {
   const elem = document.createElement('div');
   ReactDOM.render(<App />, elem);
 });
-
-it("Renders Navigation Bar/TopBar", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/home/i);
-  const secondLinkElement = screen.getByText(/carousel/i);
-  expect(linkElement).toBeInTheDocument();
-  expect(secondLinkElement).toBeInTheDocument();
-});
