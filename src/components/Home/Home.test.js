9382950de6ed3ce10a3ef8b80c8de52c68eca7f3
Hari Veturi <hariveturi@gmail.com>
2021-02-24 02:56:48 +0530

    ADD: Test from app has been moved to home

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,3 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import Home from './Home';
+
+it("Renders without crashing", () => {
+  const elem = document.createElement('div');
+  ReactDOM.render(<Home />, elem);
+});
+
+it("Renders Navigation Bar/TopBar", () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/home/i);
+  const secondLinkElement = screen.getByText(/carousel/i);
+  expect(linkElement).toBeInTheDocument();
+  expect(secondLinkElement).toBeInTheDocument();
+});
