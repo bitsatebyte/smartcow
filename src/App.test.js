f00122ee2c769124de34537c8614ffd439f876df
Hari Veturi <hariveturi@gmail.com>
2021-02-24 02:48:30 +0530

    ADD: Test for carousel link in navbar to the existing test case

--- src/App.test.js
+++ src/App.test.js
@@ -11,5 +11,7 @@ it("Renders without crashing", () => {
 it("Renders Navigation Bar/TopBar", () => {
   render(<App />);
   const linkElement = screen.getByText(/home/i);
+  const secondLinkElement = screen.getByText(/carousel/i);
   expect(linkElement).toBeInTheDocument();
+  expect(secondLinkElement).toBeInTheDocument();
 });
