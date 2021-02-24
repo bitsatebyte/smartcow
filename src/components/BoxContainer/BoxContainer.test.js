8250e952de20eb176390e2f194ad4c0878441a99
Hari Veturi <hariveturi@gmail.com>
2021-02-25 02:14:48 +0530

    ADD: Test for BoxContainer component

--- src/components/BoxContainer/BoxContainer.test.js
+++ src/components/BoxContainer/BoxContainer.test.js
@@ -1,3 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+
+import BoxContainer from './BoxContainer';
+
+it("Renders Box Container without crashing", () => {
+  render(<BoxContainer />);
+  const inputElem = screen.getByRole('textbox', { name: /box-input/i })
+  expect(inputElem).toBeInTheDocument();
+});
