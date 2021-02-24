6273e030246c3092fcc758c067350decb82c5fff
Hari Veturi <hariveturi@gmail.com>
2021-02-25 01:13:49 +0530

    ADD: Tests for YoutubeVideo Component
    data-testid as yt-video was added to YoutubeVideo component for the
    purpose of testing.

--- src/components/YoutubeVideo/YoutubeVideo.js
+++ src/components/YoutubeVideo/YoutubeVideo.js
@@ -5,7 +5,8 @@ function YoutubeVideo() {
     <div className="youtubeVideo">
       <iframe width="300" height="150"
         src="https://www.youtube-nocookie.com/embed/WbWhpfXisZw"
-        title="some"
+        title="porcupine-tree"
+        data-testid="yt-video"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen>
--- src/components/YoutubeVideo/YoutubeVideo.test.js
+++ src/components/YoutubeVideo/YoutubeVideo.test.js
@@ -1,3 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
+
+import YoutubeVideo from './YoutubeVideo';
+
+
+it("Renders iframe element without crashing", () => {
+  const elem = document.createElement('div');
+  ReactDOM.render(<YoutubeVideo />, elem);
+});
+
+it("Renders a youtube video", () => {
+  const { getByTestId } = render(<YoutubeVideo />);
+  expect(getByTestId('yt-video')).toBeInTheDocument();
+});
