18b9c881aab2e7e7e78f9665b0922860e9ea8df7
Hari Veturi <hariveturi@gmail.com>
2021-02-25 02:13:41 +0530

    ADD: Test for Carousel text

--- src/components/TextOnCarousel/TextOnCarousel.test.js
+++ src/components/TextOnCarousel/TextOnCarousel.test.js
@@ -1,3 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
+
+import TextOnCarousel from './TextOnCarousel';
+
+
+it("Renders text without crashing", () => {
+  const elem = document.createElement('div');
+  ReactDOM.render(<TextOnCarousel />, elem);
+});
+
+it("Renders the header element", () => {
+  const { getByRole } = render(<TextOnCarousel />);
+  expect(getByRole('heading')).toBeInTheDocument();
+});
