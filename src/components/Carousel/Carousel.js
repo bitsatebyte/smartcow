08eef14500ebe00a853d7d669bafc9f583dbf09e
Hari Veturi <hariveturi@gmail.com>
2021-02-25 20:53:06 +0530

    FIX: Removed complementary colour calc function
    This extensive implementation for dynamic colours was not
    required.
    It was achieved using filter: brightness(0.5) on the image;

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,7 +1,5 @@
 import { useEffect } from 'react';
 import Flickity from 'flickity';
-import analyze from 'rgbaster';
-import tinycolor from 'tinycolor2';
 import 'flickity/css/flickity.css';
 import './Carousel.css';
 
@@ -10,14 +8,6 @@ import firstImage from '../../static/images/1.jpg';
 import secondImage from '../../static/images/2.jpg';
 import thirdImage from '../../static/images/3.jpg';
 import fourthImage from '../../static/images/4.jpg';
-
-async function calculateComplementaryColorForBanner(path) {
-  const c = await analyze(path, { ignore: ['rgb(255, 255, 255)', 'rgb(0, 0, 0)'] });
-  const [primaryRgb, secondaryRgb, tertiaryRgb] = [c[0].color, c[1].color, c[2].color];
-  console.log(`Primary: ${primaryRgb} \n Secondary: ${secondaryRgb} \n Tertiary: ${tertiaryRgb}`);
-  const complement = tinycolor(primaryRgb.toString()).complement().toHexString();
-  console.log(complement);
-}
 
 const mapImagesToDivs = arr => {
   const ret = arr.map((el, index) => {
@@ -43,7 +33,6 @@ function Carousel() {
       contain: true,
       pageDots: false
     });
-    calculateComplementaryColorForBanner(fourthImage);
   }, []);
 
   return (
