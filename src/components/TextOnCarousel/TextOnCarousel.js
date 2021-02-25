c231fb7a48a8239153734dfd549282babf1877b4
Hari Veturi <hariveturi@gmail.com>
2021-02-25 21:09:13 +0530

    ENH: Change text on carousel from h2 to h1

--- src/components/TextOnCarousel/TextOnCarousel.js
+++ src/components/TextOnCarousel/TextOnCarousel.js
@@ -2,7 +2,7 @@ import './TextOnCarousel.css';
 
 function TextOnCarousel() {
   return (
-    <h2 id="carousel-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, architecto!</h2>
+    <h1 id="carousel-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, architecto!</h1>
   );
 }
 
